7f9ef93be33e45f350ea7782adcfc8637db42193
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-04-18 20:20:50 -0300

    add room list and room owner id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,18 @@ import Link from 'next/link'
 
 import { buttonVariants } from '@/components/button'
 import { getCurrentUser } from '@/lib/auth'
+import { db } from '@/lib/db'
 
 import { CreateRoom } from './room/components/create-room'
 
 export default async function IndexPage() {
   const user = await getCurrentUser()
+
+  const rooms = await db.room.findMany({
+    where: {
+      ownerId: user!.id,
+    },
+  })
 
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center px-4">
@@ -31,6 +38,22 @@ export default async function IndexPage() {
           Sign in
         </Link>
       )}
+
+      <ul className="mt-10 space-y-4">
+        {rooms.map((room) => (
+          <li key={room.id}>
+            <Link
+              className={buttonVariants({
+                variant: 'outline',
+                className: 'w-full',
+              })}
+              href={`/room/${room.id}`}
+            >
+              {room.id}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,8 +1,27 @@
 import { Board } from '@/components/board'
+import { db } from '@/lib/db'
 
-export default function RoomPage() {
+type RoomPageProps = {
+  params: {
+    roomId: string
+  }
+}
+
+export default async function RoomPage({ params }: RoomPageProps) {
+  const roomId = params.roomId as string
+
+  const room = await db.room.findUnique({
+    where: {
+      id: roomId,
+    },
+  })
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center px-4">
+      <div className="mb-10">
+        Room owner ID: <strong>{room?.ownerId}</strong>
+      </div>
+
       <section className="max-w-lg">
         <Board />
       </section>
